3d60182c4cc5c338efaceeceea9e19e39778c2ff
andrew <Andrew.j.troup@gmail.com>
2017-10-04 22:02:45 -0700

    initila commit into master
    pushign what i have up to this date. Need to get the gridview up and working tomorrow.

---
+++ QwikAx/app/src/main/java/cmps121/qwikax/MainActivity.java
@@ -0,0 +1,66 @@
+package cmps121.qwikax;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // FIELDS
+    private GridView _gridView;
+    // FIELDS
+
+    // ENUMS
+
+    // ENUMS
+
+    // METHODS
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        _gridView = (GridView) findViewById(R.id.gridView);
+
+        _gridView.setAdapter(new CustomGridAdapter(this, 3, 4));
+        _gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(), "row: "+ position / 3 + "\ncolumn:" + position % 3, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // METHODS
+}
