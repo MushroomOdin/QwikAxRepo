e2a2982d098032a3adee04ae5a8b1779241f0364
Michael Crane <mbcrane@ucsc.edu>
2017-10-21 18:08:56 -0700

    built notification class, crashes when used though

--- app/src/main/java/cmps121/qwikax/Pulldown_Notification/PulldownNotification.java
+++ app/src/main/java/cmps121/qwikax/Pulldown_Notification/PulldownNotification.java
@@ -1,9 +1,34 @@
 package cmps121.qwikax.Pulldown_Notification;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+
+
+import cmps121.qwikax.MainActivity;
+import cmps121.qwikax.R;
+
 /**
  * Created by Michael Crane on 10/21/2017.
  */
 
-public class PulldownNotification {
+public class PulldownNotification extends MainActivity {
+
+    public void sendNotification(View view){
+        //instance of notificationManager
+        NotificationCompat.Builder myBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("test notification")
+                .setContentText("suprise!");
+        //instance of service
+        NotificationManager myNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        //NotificationManager.notify().
+        myNotificationManager.notify(001, myBuilder.build());
+    }
+
 
 }
