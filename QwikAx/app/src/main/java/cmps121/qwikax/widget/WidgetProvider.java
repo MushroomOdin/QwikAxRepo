6aa3d79b4c3f9048680d24c3f37801de79895425
Juvenal Arango <juve2527@gmail.com>
2017-12-04 18:11:18 -0800

    Widget Progress Save
    Modified Manifest
    Created appwidget.xml
    Created appwidget_info.xml
    Created WidgetProvider.java
    Still working on WidgetProvider

---
+++ QwikAx/app/src/main/java/cmps121/qwikax/widget/WidgetProvider.java
@@ -0,0 +1,18 @@
+package cmps121.qwikax.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Juve on 12/4/2017.
+ */
+
+public class WidgetProvider extends AppWidgetProvider{
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        int appWidgetId = appWidgetIds[0];
+
+        Intent intent = new Intent(context,)
+    }
+}
