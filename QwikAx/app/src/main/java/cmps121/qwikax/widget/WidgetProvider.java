5bae0b2b987dc1454d5fc917686bb0366a74b36b
Juvenal Arango <juve2527@gmail.com>
2017-12-05 11:28:47 -0800

    Modified Widget Provider
    Updated widget provider, this file should be done now, but need to work of RemoteViewsFactory

--- QwikAx/app/src/main/java/cmps121/qwikax/widget/WidgetProvider.java
+++ QwikAx/app/src/main/java/cmps121/qwikax/widget/WidgetProvider.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
 
+import java.util.List;
+
 import cmps121.qwikax.MainActivity;
 import cmps121.qwikax.R;
 
@@ -15,10 +17,33 @@ import cmps121.qwikax.R;
  * Created by Juve on 12/4/2017.
  */
 
+
 public class WidgetProvider extends AppWidgetProvider{
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds){
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onDisabled(Context context){
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onEnabled(Context context){
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        super.onReceive(context, intent);
+    }
+
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
-        int N = appWidgetIds.length;
-        for(int i=0; i<N; i++){
+
+        for(int i=0; i<appWidgetIds.length; i++){
             int appWidgetId = appWidgetIds[i];
             Intent intent=new Intent(context, WidgetService.class);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
@@ -30,10 +55,10 @@ public class WidgetProvider extends AppWidgetProvider{
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
             views.setRemoteAdapter(R.id.WidgetListView, intent);
 
-            Intent clickIntent= new Intent(context, MainActivity.class);
+            /*Intent clickIntent= new Intent(context, MainActivity.class);
             PendingIntent clickPend = PendingIntent.getActivity(context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            views.setPendingIntentTemplate(R.id.words, clickPend);
+            views.setPendingIntentTemplate(R.id.words, clickPend);*/
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
